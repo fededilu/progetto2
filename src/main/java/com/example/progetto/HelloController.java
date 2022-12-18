cc7b1c3cc7bd65b49dfe1ec9e9068e4eb9df5468
plaias <federicodiluzio@hotmail.it>
2022-12-18 18:57:05 +0100

    fine

--- src/main/java/com/example/progetto/HelloController.java
+++ src/main/java/com/example/progetto/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.progetto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private String email = "";
+    private String password = "";
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label testoInvio;
+
+    @FXML
+    private TextField boxEmail;
+
+    @FXML
+    private PasswordField boxPassword;
+    @FXML
+    private Button canella;
+
+    @FXML
+    private Button invia;
+
+    @FXML
+    void cancella(ActionEvent event) {
+        boxPassword.setText("");
+        boxEmail.setText("");
+    }
+
+    @FXML
+    void invia(ActionEvent event) {
+        email = boxEmail.getText();
+        System.out.println(email);
+        testoInvio.setText(email);
     }
 }
